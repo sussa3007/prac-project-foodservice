d32d695a8f1de7835ebde4ce8643c9fa45f072fd
sussa3007 <sussa1933@gmail.com>
2022-11-12 18:51:25 +0900

    Set #1 - Application

---
+++ src/test/java/com/example/mappingprac/MappingpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mappingprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
