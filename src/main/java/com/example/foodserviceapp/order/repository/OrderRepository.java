d0bf543b1712bfa91ab4fb8f4e67aa0ee115ebc0
sussa3007 <sussa1933@gmail.com>
2022-11-18 01:14:25 +0900

    Refactor #4 - 1+N Issue
    # 1+N 문제 해결하기 위해 fatch join 시도
    # 페이징 기능 추가하기 위해 컬렉션 컬럼은 제외하고 지연로딩 컬럼만 fatch join하고 카운트쿼리 추가
    # 지정 멤버에 대한 오더 리스트 조회 추가

--- src/main/java/com/example/foodserviceapp/order/repository/OrderRepository.java
+++ src/main/java/com/example/foodserviceapp/order/repository/OrderRepository.java
@@ -3,11 +3,42 @@ package com.example.foodserviceapp.order.repository;
 import com.example.foodserviceapp.member.entity.Member;
 import com.example.foodserviceapp.order.entity.Order;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
-    Page<Order> findAllByOrderByOrderIdDesc(Pageable pageable);
+import java.util.List;
 
+public interface OrderRepository extends
+        JpaRepository<Order,Long>
+{
+
+
+    /* 지정 Member의 Order List 조회 */
     Page<Order> findByMemberOrderByOrderIdDesc(Member member, Pageable pageable);
+
+
+    /**
+     * TODO 두개 이상의 컬랙션은 페치조인으로 조회 하지 못한다.
+     * 두개의 컬렉션에 페치 조인 할 경우 예외 정리
+     * Option은 왜 해결되지 못하지?
+     * @return
+     */
+    @Query("select o from ORDERS o " +
+//            "join fetch o.options op " +
+            "join fetch o.member m  " +
+            "join fetch m.point pt " +
+            "join fetch o.orderFoods od " +
+            "join fetch od.food f " +
+            "order by o.orderId desc")
+    List<Order> findAllQuery();
+
+
+    @Query(value = "select o from ORDERS o " +
+            "join fetch o.member m " +
+            "join fetch m.point p",
+            countQuery = "SELECT COUNT(o) FROM ORDERS o")
+    Page<Order> findAllByOrder(Pageable pageable);
+
 }
